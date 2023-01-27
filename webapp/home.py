ae5185d2adafdda233f7bed0f5d25446159a4f4e
aysebat <aaysebatt@gmail.com>
2023-01-27 08:58:13 +0300

    Setting the home div for the element

--- webapp/home.py
+++ webapp/home.py
@@ -3,3 +3,13 @@ import justpy as jp
 
 @jp.SetRoute("/home")
 def home():
+    wp = jp.QuasarPage(tailwind=True)
+    container = jp.Div(a=wp, classes="bg-gray-200 h-screen")
+
+    jp.Div(a=container, text="Div Content")
+    return wp
+
+
+
+#to run the server
+jp.justpy()
