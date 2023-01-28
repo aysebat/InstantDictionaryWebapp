4d57ee2e39f035588b371b57db737981d7b85829
aysebat <aaysebatt@gmail.com>
2023-01-28 20:33:04 +0300

    change the get function to teh serve function

--- webapp/home.py
+++ webapp/home.py
@@ -1,15 +1,14 @@
 import justpy as jp
 
 
-@jp.SetRoute("/home")
-def home():
-    wp = jp.QuasarPage(tailwind=True)
-    container = jp.Div(a=wp, classes="bg-gray-200 h-screen")
 
-    jp.Div(a=container, text="Div Content")
-    return wp
+class Home:
+    path ='/home'
+    def serve(self):
+        wp = jp.QuasarPage(tailwind=True)
+        mainDiv = jp.Div(a=wp, classes="bg-gray-200 h-screen")
+
+        jp.Div(a=mainDiv, text="Div Content")
+        return wp
 
 
-
-#to run the server
-jp.justpy()
