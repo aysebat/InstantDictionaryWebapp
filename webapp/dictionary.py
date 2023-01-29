f38518331172bd0ae0af48bcdea193aaba1b6d86
aysebat <aaysebatt@gmail.com>
2023-01-29 14:23:22 +0300

    add description and input

--- webapp/dictionary.py
+++ webapp/dictionary.py
@@ -10,5 +10,13 @@ class Dictionary:
         jp.Div(a=mainDiv,
                text="Instant English Dictionary",
                classes="text-4xl m-2")
+        jp.Div(a=mainDiv,
+               text="Get the definition of any English word instantly as you type...",
+               clases="text-lg")
+        jp.Input(a=mainDiv,
+                 placeholder="Type in any word here...",
+                 classes="m-2 bg-gray-200 border-2 border-gray-200 rounded w-64 "
+                         "focus:outline-none focus:border-purple-500 py-2 px-4")
         return wp
+
 
