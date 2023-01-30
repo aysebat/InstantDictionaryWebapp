0a80e07c70d7ea2590e8a118fbd746d1f3d7ab07
aysebat <aaysebatt@gmail.com>
2023-01-30 20:17:33 +0300

    added the font mono

--- webapp/dictionary.py
+++ webapp/dictionary.py
@@ -7,7 +7,7 @@ class Dictionary:
     @classmethod
     def serve(cls, req):
         wp = jp.QuasarPage(tailwind=True)
-        mainDiv = jp.Div(a=wp, classes="bg-gray-00 h-screen")
+        mainDiv = jp.Div(a=wp, classes="bg-gray-00 h-screen font-mono")
         jp.Div(a=mainDiv,
                text="Instant English Dictionary",
                classes="text-4xl m-2")
@@ -20,7 +20,7 @@ class Dictionary:
                  placeholder="Type in any word here...",
                  classes="m-2 bg-gray-100 border-2 border-gray-200 rounded w-64 "
                          "focus:outline-none focus:border-purple-500 focus:bg-white "
-                         "py-2 px-4")
+                         "py-2 px-4 font-mono")
 
         outputDiv = jp.Div(a=mainDiv,
                            classes="m-2 p-2 text-lg border-2 h-40 ")
@@ -35,8 +35,8 @@ class Dictionary:
     @staticmethod
     def get_definition(widget, msg):
         """widget is the button
-           msg is the contain the data about the event"""
-        definition = Definition(term=widget.inputbox.value).get()
-        widget.outputDiv.text = definition
+           msg is contain the data about the event"""
+        defined = Definition(widget.inputbox.value).get()
+        widget.outputDiv.text = defined
 
 
