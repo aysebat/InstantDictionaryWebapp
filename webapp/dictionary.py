2deb94ecc78046e73bb7bf08c466cfc76a6937d2
aysebat <aaysebatt@gmail.com>
2023-01-30 21:04:09 +0300

    postpone the get defination not working for now

--- webapp/dictionary.py
+++ webapp/dictionary.py
@@ -1,5 +1,5 @@
 import justpy as jp
-from definition import Definition
+import definition
 
 class Dictionary:
     path = "/dictionary"
@@ -10,17 +10,17 @@ class Dictionary:
         mainDiv = jp.Div(a=wp, classes="bg-gray-00 h-screen font-mono")
         jp.Div(a=mainDiv,
                text="Instant English Dictionary",
-               classes="text-4xl m-2")
+               classes="text-4xl m-2 ")
         jp.Div(a=mainDiv,
                text="Get the definition of any English word instantly as you type...",
                clases="text-lg m-2")
         #input div
-        inputDiv = jp.Div(a=mainDiv, classes="grid grid-cols-2")
+        inputDiv = jp.Div(a=mainDiv, classes="grid grid-cols-2 my-4")
         inputBox = jp.Input(a=inputDiv,
-                 placeholder="Type in any word here...",
-                 classes="m-2 bg-gray-100 border-2 border-gray-200 rounded w-64 "
-                         "focus:outline-none focus:border-purple-500 focus:bg-white "
-                         "py-2 px-4 font-mono")
+                            placeholder="Type in any word here...",
+                            classes="m-2 bg-gray-100 border-2 border-gray-200 rounded w-64 "
+                                    "focus:outline-none focus:border-purple-500 focus:bg-white "
+                                    "py-2 px-4 font-mono")
 
         outputDiv = jp.Div(a=mainDiv,
                            classes="m-2 p-2 text-lg border-2 h-40 ")
@@ -28,15 +28,18 @@ class Dictionary:
                   text="GetDefinition",
                   classes="border-2 text-gray-s500",
                   click=cls.get_definition, output=outputDiv, input=inputBox)
-        #############
+                  #you can use output and input variable in get_definition method
+        ############# ,
 
+        print(cls, req)
         return wp
 
     @staticmethod
     def get_definition(widget, msg):
         """widget is the button
            msg is contain the data about the event"""
-        defined = Definition(widget.inputbox.value).get()
-        widget.outputDiv.text = defined
+
+        #defined = definition.Definition(widget.input.value).get()
+        #widget.output.text = widget.input.value
 
 
