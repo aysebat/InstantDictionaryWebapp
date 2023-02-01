c04077cf092862f90c23893d9e7cde752e851c62
aysebat <aaysebatt@gmail.com>
2023-02-01 18:40:29 +0300

    Update the get definaition

--- webapp/dictionary.py
+++ webapp/dictionary.py
@@ -1,6 +1,7 @@
 import justpy as jp
 import definition
 
+
 class Dictionary:
     path = "/dictionary"
 
@@ -14,24 +15,25 @@ class Dictionary:
         jp.Div(a=mainDiv,
                text="Get the definition of any English word instantly as you type...",
                clases="text-lg m-2")
-        #input div
+        # input div
         inputDiv = jp.Div(a=mainDiv, classes="grid grid-cols-2 my-4")
-        input_box = jp.Input(a=inputDiv,
-                            placeholder="Type in any word here...",
-                            classes="m-2 bg-gray-100 border-2 border-gray-200 rounded w-64 "
-                                    "focus:outline-none focus:border-purple-500 focus:bg-white "
-                                    "py-2 px-4 font-mono")
-
-        #when someone write on the input box
-        input_box.on('input', cls.get_definition)
-
         outputDiv = jp.Div(a=mainDiv,
                            classes="m-2 p-2 text-lg border-2 h-40 ")
+        input_box = jp.Input(a=inputDiv,
+                             placeholder="Type in any word here...",
+                             classes="m-2 bg-gray-100 border-2 border-gray-200 rounded w-64 "
+                                     "focus:outline-none focus:border-purple-500 focus:bg-white "
+                                     "py-2 px-4 font-mono",
+                             output_div=outputDiv)
+
+        # when someone write on the input box
+        input_box.on('input', cls.get_definition)
+
         # jp.Button(a=inputDiv,
         #           text="GetDefinition",
         #           classes="border-2 text-gray-s500",
         #           click=cls.get_definition,
-        #           output=outputDiv,
+        #           output_div=outputDiv,
         #           input_box=input_box
         #           )
         #           #you can use output and input variable in get_definition method
@@ -45,10 +47,7 @@ class Dictionary:
            msg is contain the data about the event"""
 
         defined = definition.Definition(widget.value).get()
-        widget.output.text = defined
+        widget.output_div.text = " ".join(defined)
 
         # this can be used for teh button elemnt
         # defined = definition.Definition(widget.input.value).get()
-
-
-
