12835e4a064245b108d3328242e6f3bf1a83d037
aysebat <aaysebatt@gmail.com>
2023-02-01 21:16:19 +0300

    home page quasr degign

--- webapp/home.py
+++ webapp/home.py
@@ -12,7 +12,8 @@ class Home:
                             classses="bg-primary text-white font-mono")
         toolbar = jp.QToolbar(a=header)
 
-        drawer = jp.QDrawer(show_if_above=True,
+        drawer = jp.QDrawer(a=layout,
+                            show_if_above=True,
                             v_model="leftDrawerOpen",
                             side="left",
                             bordered=True)
@@ -26,7 +27,7 @@ class Home:
         jp.QToolbarTitle(a=toolbar,
                          text="Instant Dictionary")
         container = jp.QPageContainer(a=layout)
-        mainDiv = jp.Div(a=container, classes="bg-gray-200 h-screen")
+        mainDiv = jp.Div(a=container, classes="h-screen")
 
         jp.Div(a=mainDiv, text="This is the Home Page")
         return wp
