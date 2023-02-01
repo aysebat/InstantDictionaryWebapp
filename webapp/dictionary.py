d421e8133c7149da9eacbe9cebbfab3269addd00
aysebat <aaysebatt@gmail.com>
2023-02-01 18:00:38 +0300

    adde  teh instatnt method

--- webapp/dictionary.py
+++ webapp/dictionary.py
@@ -22,9 +22,19 @@ class Dictionary:
                                     "focus:outline-none focus:border-purple-500 focus:bg-white "
                                     "py-2 px-4 font-mono")
 
+        #when someone write on the input box
+        input_box.on('input', cls.get_definition)
+
         outputDiv = jp.Div(a=mainDiv,
                            classes="m-2 p-2 text-lg border-2 h-40 ")
-
+        # jp.Button(a=inputDiv,
+        #           text="GetDefinition",
+        #           classes="border-2 text-gray-s500",
+        #           click=cls.get_definition,
+        #           output=outputDiv,
+        #           input_box=input_box
+        #           )
+        #           #you can use output and input variable in get_definition method
 
         print(cls, req)
         return wp
@@ -33,7 +43,10 @@ class Dictionary:
     def get_definition(widget, msg):
         """widget is the button
            msg is contain the data about the event"""
-        #defined = definition.Definition(widget.input.value).get()
-        widget.output.text = widget.input.value
+
+
+        #this can be used for teh button elemnt
+        # defined = definition.Definition(widget.input.value).get()
+        # widget.output.text = defined
 
 
