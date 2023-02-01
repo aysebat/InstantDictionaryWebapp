9f155864df33f9fa4eeefb0fcd70cdb61c6bf854
aysebat <aaysebatt@gmail.com>
2023-02-01 21:13:19 +0300

    update

--- webapp/home.py
+++ webapp/home.py
@@ -4,40 +4,36 @@ import justpy as jp
 class Home:
     path = "/"
 
-     @classmethod
+    @classmethod
     def serve(cls, req):
         wp = jp.QuasarPage(tailwind=True)
         layout = jp.QLayout(a=wp, view="hHh lpR fFf")
         header = jp.QHeader(a=layout,
-                            classses="bg-primary text-white")
+                            classses="bg-primary text-white font-mono")
         toolbar = jp.QToolbar(a=header)
-
-
-        jp.QToolbarTitle(a=toolbar,
-                         text="Instant Dictionary")
 
         drawer = jp.QDrawer(show_if_above=True,
                             v_model="leftDrawerOpen",
                             side="left",
                             bordered=True)
-
         jp.QBtn(a=toolbar,
-                dense=True,
                 flat=True,
+                dense=True,
                 round=True,
                 icon="menu",
-                click=cls.move_draver, drawer=drawer)
-
-
-        mainDiv = jp.Div(a=wp, classes="bg-gray-200 h-screen")
+                click=cls.move_drawer,
+                drawer=drawer)
+        jp.QToolbarTitle(a=toolbar,
+                         text="Instant Dictionary")
+        container = jp.QPageContainer(a=layout)
+        mainDiv = jp.Div(a=container, classes="bg-gray-200 h-screen")
 
         jp.Div(a=mainDiv, text="This is the Home Page")
         return wp
-@staticmethod
-def move_draver(wigdet, msg):
-    if widget.drawer.value:
-        widget.drawer.value = False
-    else:
-        widget.drawer.value = True
 
-
+    @staticmethod
+    def move_drawer(widget, msg):
+        if widget.drawer.value:
+            widget.drawer.value = False
+        else:
+            widget.drawer.value = True
