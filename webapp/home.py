57f203f0d2ef62f6b7e9d3166425eb2c5fab298a
aysebat <aaysebatt@gmail.com>
2023-02-02 07:39:05 +0300

    update the hoem with DefaultLayout Class

--- webapp/home.py
+++ webapp/home.py
@@ -1,5 +1,5 @@
 import justpy as jp
-
+import layout
 
 class Home:
     path = "/"
@@ -7,44 +7,11 @@ class Home:
     @classmethod
     def serve(cls, req):
         wp = jp.QuasarPage(tailwind=True)
-        layout = jp.QLayout(a=wp, view="hHh lpR fFf")
-        header = jp.QHeader(a=layout,
-                            classses="bg-primary text-white font-mono")
-        toolbar = jp.QToolbar(a=header)
-
-        drawer = jp.QDrawer(a=layout,
-                            show_if_above=True,
-                            v_model="leftDrawerOpen",
-                            side="left",
-                            bordered=True)
-        scroller = jp.QScrollArea(a=drawer, classes="fit")
-        qlist = jp.QList(a=scroller)
-        a_classes = 'p-2 n-2 text-lg text-blue-400 hover:text-blue-700'
-        jp.A(a=qlist, text="Home", href="/", classes=a_classes)
-        jp.Br(a=qlist)
-        jp.A(a=qlist, text="About", href="/about" , classes=a_classes)
-        jp.Br(a=qlist)
-        jp.A(a=qlist, text="Dictionary", href="\dictionary" , classes=a_classes)
-        jp.Br(a=qlist)
-
-        jp.QBtn(a=toolbar,
-                flat=True,
-                dense=True,
-                round=True,
-                icon="menu",
-                click=cls.move_drawer,
-                drawer=drawer)
-        jp.QToolbarTitle(a=toolbar,
-                         text="Instant Dictionary")
-        container = jp.QPageContainer(a=layout)
+        web_layout = layout.DefaultLayout(a=wp, view="hHh lpR fFf")
+        container = jp.QPageContainer(a=web_layout)
         mainDiv = jp.Div(a=container, classes="h-screen")
 
         jp.Div(a=mainDiv, text="This is the Home Page")
         return wp
 
-    @staticmethod
-    def move_drawer(widget, msg):
-        if widget.drawer.value:
-            widget.drawer.value = False
-        else:
-            widget.drawer.value = True
+
